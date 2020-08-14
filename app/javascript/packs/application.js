0c836426a0b0af946825e01f3ff2be31da4926fc
joseph thomas <up2josephthomas@gmail.com>
2020-08-14 19:27:18 +0530

    yarn works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,14 @@ require("channels")
 
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
+import '@material/icon-button/dist/mdc.icon-button';
